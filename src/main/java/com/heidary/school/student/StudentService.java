db1a30d6d0eeccd7920c43999a56a40cc00d1333
Sepehr Heidary <mhd71313@gmail.com>
2023-01-01 21:45:35 +0330

    PUT added

--- src/main/java/com/heidary/school/student/StudentService.java
+++ src/main/java/com/heidary/school/student/StudentService.java
@@ -3,9 +3,11 @@ package com.heidary.school.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -17,22 +19,38 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentRepository.findAll();
     }
 
     public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
         if (studentOptional.isPresent()) {
-            throw  new IllegalStateException("email already exists!");
+            throw new IllegalStateException("email already exists!");
         }
         studentRepository.save(student);
     }
 
     public void deleteStudent(Long studentId) {
-        if(!studentRepository.existsById(studentId)){
+        if (!studentRepository.existsById(studentId)) {
             throw new IllegalStateException(studentId + " id does`nt exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(studentId + " id does`nt exist"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getfName(), name)) {
+            student.setfName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
