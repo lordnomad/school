b281773858d7dbbba5e41f00d385b7dfc9d3dccd
Sepehr Heidary <mhd71313@gmail.com>
2023-01-01 19:39:25 +0330

    POST ADDED

--- src/main/java/com/heidary/school/student/StudentRepository.java
+++ src/main/java/com/heidary/school/student/StudentRepository.java
@@ -1,8 +1,15 @@
 package com.heidary.school.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    //for checking if an email exist or it`s new
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
